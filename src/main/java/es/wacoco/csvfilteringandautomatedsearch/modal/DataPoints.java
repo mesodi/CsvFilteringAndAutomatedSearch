60362fb5b566bc469daea2cfb388b5096732f5ed
mariem <mariem.saleh@hotmail.com>
2024-02-20 15:03:07 +0100

    DataPoints

--- src/main/java/es/wacoco/csvfilteringandautomatedsearch/modal/DataPoints.java
+++ src/main/java/es/wacoco/csvfilteringandautomatedsearch/modal/DataPoints.java
@@ -1,4 +1,27 @@
 package es.wacoco.csvfilteringandautomatedsearch.modal;
 
 public class DataPoints {
+
+    private String inventors;
+    private String applicationDate;
+    private String applicationNumber;
+    private String applicant;
+    private String title;
+    private String abstractText;
+
+    public DataPoints(String  inventors, String applicationDate,String applicationNumber,
+                      String applicant,String title,String abstractText){
+
+        this.inventors = inventors;
+        this.applicationDate = applicationDate;
+        this.applicationNumber = applicationNumber;
+        this.applicant = applicant;
+        this.title = title;
+        this.abstractText = abstractText;
+
+
+
+
+    }
+
 }
