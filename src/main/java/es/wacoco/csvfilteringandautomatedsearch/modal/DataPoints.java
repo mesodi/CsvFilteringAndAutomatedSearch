db61a330e35b2d81db6202a7eb7b743c7de73069
mariem <mariem.saleh@hotmail.com>
2024-02-20 22:34:38 +0100

    +

--- src/main/java/es/wacoco/csvfilteringandautomatedsearch/modal/DataPoints.java
+++ src/main/java/es/wacoco/csvfilteringandautomatedsearch/modal/DataPoints.java
@@ -8,9 +8,12 @@ public class DataPoints {
     private String applicant;
     private String title;
     private String abstractText;
+    private String companies;
 
-    public DataPoints(String  inventors, String applicationDate,String applicationNumber,
-                      String applicant,String title,String abstractText){
+
+    public DataPoints(String  inventors, String applicationDate, String applicationNumber,
+                      String applicant, String title, String abstractText,String
+                       companies){
 
         this.inventors = inventors;
         this.applicationDate = applicationDate;
@@ -18,10 +21,51 @@ public class DataPoints {
         this.applicant = applicant;
         this.title = title;
         this.abstractText = abstractText;
-
-
-
+        this.companies= companies;
 
     }
+    public String getInventors() {
+        return inventors;
+    }
+    public void setInventors(String inventors) {
+        this.inventors = inventors;
+    }
+    public String getApplicationDate() {
+        return applicationDate;
+    }
+    public void setApplicationDate(String applicationDate) {
+        this.applicationDate = applicationDate;
+    }
+    public String getApplicationNumber() {
+        return applicationNumber;
+    }
+    public void setApplicationNumber(String applicationNumber) {
+        this.applicationNumber = applicationNumber;
+    }
+    public String getApplicant() {
+        return applicant;
+    }
+    public void setApplicant(String applicant) {
+        this.applicant = applicant;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
+    public String getAbstractText() {
+        return abstractText;
+    }
+    public void setAbstractText(String abstractText) {
+        this.abstractText = abstractText;
+    }
+    public String getCompanies() {
+        return companies;
+    }
+
+    public void setCompanies(String companies) {
+        this.companies = companies;
+    }
 }
