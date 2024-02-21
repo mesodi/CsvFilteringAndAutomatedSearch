9f504b259d67b46d25847957f3aa0773dc39f7bb
Rufus100Procent <Maxamadabdulahist@outlook.com>
2024-02-21 11:45:50 +0100

    Added inventorname

--- src/main/java/es/wacoco/csvfilteringandautomatedsearch/model/Linkedin.java
+++ src/main/java/es/wacoco/csvfilteringandautomatedsearch/model/Linkedin.java
@@ -1,10 +1,17 @@
 package es.wacoco.csvfilteringandautomatedsearch.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
+@AllArgsConstructor
+@Setter
 public class Linkedin {
-
+    private String inventorName;
     private List<String> linkedinUrl = new ArrayList<>();
 
 }
