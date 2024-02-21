85bb9f7f32d6358642cb6a5909dfcb49c7164a7a
Rufus100Procent <Maxamadabdulahist@outlook.com>
2024-02-21 13:48:04 +0100

    Update and creating diffrent routes

--- src/main/java/es/wacoco/csvfilteringandautomatedsearch/Camel/processor/FilterCsvProcessor.java
+++ src/main/java/es/wacoco/csvfilteringandautomatedsearch/Camel/processor/FilterCsvProcessor.java
@@ -4,7 +4,7 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.springframework.web.multipart.MultipartFile;
 
-public class FilterCsvRouteProcessor implements Processor {
+public class FilterCsvProcessor implements Processor {
     @Override
     public void process(Exchange exchange) throws Exception {
         MultipartFile file = exchange.getIn().getBody(MultipartFile.class);
--- src/main/java/es/wacoco/csvfilteringandautomatedsearch/Camel/routes/ProcessorRoute.java
+++ src/main/java/es/wacoco/csvfilteringandautomatedsearch/Camel/routes/ProcessorRoute.java
@@ -7,19 +7,20 @@ public class ProcessorRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
+        from("direct:csvFilteringRoute")
+                .process(new FilterCsvProcessor())
+                .process(new CountApplicantProcessor());
+
+
         from("direct:processorManager")
-                .process(new FilterCsvRouteProcessor())
-
-                .process(new CountApplicantProcessor())
-
                 .process(new LinkedInUrlFinderProcessor())
 
                 .process(new WebsiteUrlFinderProcessor())
 
                 .process(new EmailExtractorProcessor());
 
-
         from("direct:exportLinkedinUrlAsCsvRoute")
                 .process(new ExportLinkedinUrlAsCsvProcessor());
     }
+
 }
