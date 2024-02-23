d7de8088f6f0162ed260c1cf90cd09fb19526106
RobKaya <robkaya@icloud.com>
2024-02-23 12:20:35 +0100

    updated job function

--- src/main/java/es/wacoco/csvfilteringandautomatedsearch/Camel/database/Database.java
+++ src/main/java/es/wacoco/csvfilteringandautomatedsearch/Camel/database/Database.java
@@ -1,5 +1,21 @@
 package es.wacoco.csvfilteringandautomatedsearch.Camel.database;
 
+import es.wacoco.csvfilteringandautomatedsearch.model.Job;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Database {
 
+    ArrayList<Job> jobDB = new ArrayList<>();
+
+    public void createJob(String jobID, ) {
+        jobDB.add(new Job(null, ));
+    }
 }
--- src/main/java/es/wacoco/csvfilteringandautomatedsearch/Camel/processor/CreateJobProcessor.java
+++ src/main/java/es/wacoco/csvfilteringandautomatedsearch/Camel/processor/CreateJobProcessor.java
@@ -3,13 +3,13 @@ package es.wacoco.csvfilteringandautomatedsearch.Camel.processor;
 import es.wacoco.csvfilteringandautomatedsearch.model.Company;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public class CreateJobProcessor implements Processor {
     @Override
     public void process(Exchange exchange) throws Exception {
-        
+        List<Company> companies = exchange.getIn().getBody(List.class);
+
     }
 }
