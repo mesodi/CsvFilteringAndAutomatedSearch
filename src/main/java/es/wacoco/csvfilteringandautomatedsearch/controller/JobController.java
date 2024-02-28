79c3e38a8a7a391f4c4f2f64b2eff905e9864e2c
Bassam <shahoudbassam@gmail.com>
2024-02-28 11:23:28 +0100

    create swagger documentation

--- src/main/java/es/wacoco/csvfilteringandautomatedsearch/controller/JobController.java
+++ src/main/java/es/wacoco/csvfilteringandautomatedsearch/controller/JobController.java
@@ -1,6 +1,7 @@
 package es.wacoco.csvfilteringandautomatedsearch.controller;
 
 import es.wacoco.csvfilteringandautomatedsearch.model.Company;
+import io.swagger.v3.oas.annotations.Operation;
 import org.apache.camel.ProducerTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,7 +12,7 @@ import java.util.List;
 @RestController
 public class JobController {
     private ProducerTemplate producerTemplate;
-
+    @Operation(summary = "Create a New Job", description = "Create a new job for processing a list of companies")
     @PostMapping("/createJob")
     public String createJob(@RequestBody List<Company> companies){
         producerTemplate.sendBody("direct:processManager", companies);
